56071bdca3c01bdd926a4041165897b92bf34fed
Hafeza Mony <141733575+hafeza08mony@users.noreply.github.com>
2023-09-27 22:23:10 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+    autoplay:false,
+    autoplayTimeout: 5000,
+    animateIn:true,
+    animateOut:true,
+    responsive:{
+    
+        700:{
+            items:2
+        }
+    }
+})
